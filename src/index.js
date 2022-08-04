6eec9f851f8ac2e7c5a82ef7fb2c0057ef1482f6
srarig <starkovivan3@gmail.com>
2022-08-04 16:01:46 +0800

    Works on ie11

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import "@babel/polyfill";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
